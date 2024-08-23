86c664354be0f0d5475d5b87e2f60525c244bea4
Aditya Nair <aditya234512@gmail.com>
2024-08-24 02:01:18 +0530

    learn-wrapper-components

---
+++ React/react-in-depth/week-6-experiments/learn-wrapper_components/wrapper-cards/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <CardWrapper> hi there!</CardWrapper>
+    <CardWrapper> hi there from card2!</CardWrapper>
+
+    </>
+  )
+}
+
+function CardWrapper({children}){
+  
+  return <div style={{border:"2px solid black",padding:20}}>{children}</div>
+}
+
+
+
+export default App
