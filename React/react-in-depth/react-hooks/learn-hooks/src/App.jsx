907143523c812f34edd0721df7f0b9442676f880
Aditya Nair <aditya234512@gmail.com>
2024-08-30 09:58:30 +0530

    react-hooks

--- React/react-in-depth/react-hooks/learn-hooks/src/App.jsx
+++ React/react-in-depth/react-hooks/learn-hooks/src/App.jsx
@@ -1,32 +1,27 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 
 function App() {
 
+  const [counter,setCounter] = useState(0);
+  const [inputValue,setInputValue] = useState(1);
+
+  let count=0;
+  for(let i=1;i<=inputValue;i++){
+    count=count + i;
+  }
+  // if user input 3, compute sum from 1 to 3 and counter++
 
   return (
     <>
-    <Todo id={1}/>
+    <input onChange={function(e){
+      setInputValue(e.target.value);
+    }} placeholder='Enter a number'></input> <br></br>
+    Sum from 1 to {inputValue} is {count}
+    <br></br>
+    <button onClick={()=>{setCounter(counter+1)}}>Count is {counter}</button>
     </>
   )
 }
 
-function Todo({id}){
-    const [todo,setTodos] = useState({});
-
-    useEffect(()=>{
-      axios.get("https://sum-100xdevs.com/todos/id="+ id)
-      .then(function(response){
-        setTodos(response.data.todo)
-      })
-    },[]);
-
-    return <div>
-      {todo.title}
-
-      {todo.description}
-
-    </div>
-}
 
 export default App
